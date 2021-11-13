d317ab32dbd7a3ce4d4fe4047264e4d8df17ef3d
gulicoengineer <stp.h.happy@gmail.com>
2021-11-13 19:25:36 +0900

    create students pages function

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,26 @@ class StudentsController < ApplicationController
   end
 
   def index
+    @students = Student.all
   end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      flash[:success] = "「#{@student.last_name}」さんを登録しました！"
+      redirect_to students_new_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  private
+    def student_params
+      params.require(:student).permit(:last_name, :first_name,
+         :sex, :school_year, :class_num, :attendance_number)
+    end
 end
