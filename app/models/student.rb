77973d7f92d1edf2790b3203e8d4cfded7c12909
gulicoengineer <stp.h.happy@gmail.com>
2021-11-13 13:38:24 +0900

    create validates for students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,9 @@
 class Student < ApplicationRecord
+	validates :last_name, presence: true, length: {maximum: 25}
+	validates :first_name, presence: true, length: {maximum: 25}
+	validates :sex, presence: true, length: {maximum: 5}
+	validates :school_year, presence: true, numericality: {less_than: 7}
+	validates :class_num, presence: true, numericality: {less_than: 11}
+	validates :attendance_number, presence: true, numericality: {less_than: 51}
+	validates :seats_number, presence: true, numericality: {less_than: 51}
 end
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,87 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @student = Student.new(last_name: "江崎", first_name: "雅彦",
+       sex: "man", school_year: 6, class_num: 2, attendance_number: 6,
+        seats_number: 2)
+  end
+
+  test "should be valid" do
+    assert @student.valid?
+  end
+
+  # 存在性チェック
+  test "last_name should be present" do
+    @student.last_name = " "
+    assert_not @student.valid?
+  end
+
+  test "first_name should be present" do
+    @student.first_name = " "
+    assert_not @student.valid?
+  end
+
+  test "sex should be present" do
+    @student.sex = " "
+    assert_not @student.valid?
+  end
+
+  test "school_year should be present" do
+    @student.school_year = " "
+    assert_not @student.valid?
+  end
+
+  test "class_num should be present" do
+    @student.class_num = " "
+    assert_not @student.valid?
+  end
+
+  test "attendance_num should be present" do
+    @student.attendance_number = " "
+    assert_not @student.valid?
+  end
+
+  test "seats_num should be present" do
+    @student.seats_number = " "
+    assert_not @student.valid?
+  end
+
+  # 長さチェック
+  test "last_name should be too long" do
+    @student.last_name = "a" * 26
+    assert_not @student.valid?
+  end
+
+  test "first_name should be too long" do
+    @student.first_name = "a" * 26
+    assert_not @student.valid?
+  end
+  
+  test "sex should be too long" do
+    @student.sex = "a" * 6
+    assert_not @student.valid?
+  end
+  
+  test "school_year should be too large" do
+    @student.school_year = 7
+    assert_not @student.valid?
+  end
+  
+  test "class_num should be too large" do
+    @student.class_num = 11
+    assert_not @student.valid?
+  end
+  
+  test "attendance_number should be too large" do
+    @student.attendance_number = 51
+    assert_not @student.valid?
+  end
+  
+  test "seats_number should be too large" do
+    @student.seats_number = 51
+    assert_not @student.valid?
+  end
+  
 end
