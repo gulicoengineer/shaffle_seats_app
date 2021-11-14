94252ad2970030c94d4108504d78d94ff0eb3984
gulicoengineer <stp.h.happy@gmail.com>
2021-11-14 12:11:29 +0900

    modify to show dynamically student seats

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,9 @@
 class StaticPagesController < ApplicationController
   def main
+    @students = Student.first(30)
+    # １テーブルの席数
+    @table_line_standard_num = @students.size / 3
+    gon.students = @students
+    # debugger
   end
 end
