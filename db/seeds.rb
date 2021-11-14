f17b7a9b9840ec308bcc85356c73a8bc370e2f8b
gulicoengineer <stp.h.happy@gmail.com>
2021-11-14 12:02:11 +0900

    create sample datas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+30.times do |n|
+	l_name = "example-#{n}"
+	f_name = "hoge-#{n}"
+	sex = 
+	if n % 2 == 0
+		"男"
+	else
+		"女"
+	end
+	Student.create!(last_name: l_name, first_name: f_name, sex: sex,
+	school_year: 1, class_num: 1, attendance_number: n)
+end
