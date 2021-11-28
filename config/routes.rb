ae6296629865a45d108a2a63cbdbb9f12c6997de
gulicoengineer <stp.h.happy@gmail.com>
2021-11-28 13:21:51 +0900

    create menu and setting pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'static_pages#main'
-  get 'students/new'
+  root "static_pages#menu"
+  get 'static_pages/main', to: "static_pages#main", as:"main"
+  get "static_pages/setting", to: "static_pages#setting", as: "setting"
   resources :students
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,20 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get main" do
+  test "should get menu" do
     get root_url
     assert_response :success
   end
+
+  test "should get main" do
+    get "/static_pages/main"
+    assert_response :success
+  end
+
+  test "should get setting" do
+    get "/static_pages/setting"
+    assert_response :success
+  end
+
+  
 end
